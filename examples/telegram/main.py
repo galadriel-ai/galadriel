2f8240255ba198bb7704f95f0d4bcedbadc74f11
Isaac <isaac@galadriel.com>
2025-01-30 11:59:16 +0000

    composio tool converter and example

--- examples/telegram/main.py
+++ examples/telegram/main.py
@@ -2,8 +2,9 @@ from smolagents.models import LiteLLMModel
 from dotenv import load_dotenv
 from pathlib import Path
 
+from galadriel_agent.tools.composio_converter import convert_action
 from telegram_agent import ElonMuskAgent
-from tools import get_weather, get_time
+from tools import get_time
 from galadriel_agent.memory.memory_repository import memory_repository
 from galadriel_agent.agent import AgentRuntime
 from galadriel_agent.clients.telegram_client import TelegramClient
@@ -18,10 +19,12 @@ logger = get_agent_logger()
 
 telegram_client = TelegramClient(token=os.getenv("TELEGRAM_TOKEN"), logger=logger)
 
+composio_weather_tool = convert_action(os.getenv("COMPOSIO_API_KEY"), "WEATHERMAP_WEATHER")
+
 elon_musk_agent = ElonMuskAgent(
     memory_repository=memory_repository,
     character_json_path="./agent_configuration/example_elon_musk.json",
-    tools=[get_weather, get_time],
+    tools=[composio_weather_tool, get_time],
     model=model,
     max_steps=6,
 )
