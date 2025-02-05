c31c05a9049ed105e8dd0a61c3e6ad0c215d4933
Maciej Witowski <maciej.witowski@protocol.ai>
2025-02-05 11:57:26 +0100

    README for basic example

--- galadriel/clients/simple_message_client.py
+++ galadriel/clients/simple_message_client.py
@@ -4,7 +4,7 @@ from typing import List
 from galadriel import AgentInput, AgentOutput
 from galadriel.entities import Message, PushOnlyQueue
 
-# Implementation of agent input and output for pushing simple input for agent at specific interval
+# Implementation of agent input and output which infinitely pushes simple input for agent at specific interval
 class SimpleMessageClient(AgentInput, AgentOutput):
     def __init__(self, *messages: str, interval_seconds: int = 60):
         if not messages:
