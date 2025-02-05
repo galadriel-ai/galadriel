796fe26edc1f8551c4dc835e8709fd4a14ecd215
Han <windzhaohan@gmail.com>
2025-02-05 13:54:11 +0100

    Move web3 tools to framework

--- galadriel/tools/web3/dexscreener.py
+++ galadriel/tools/web3/dexscreener.py
@@ -2,12 +2,6 @@ import requests
 import json
 from galadriel.core_agent import tool
 
-# Predefined list of tokens with their addresses
-# TOKENS = [
-#    {"name": "DAIGE", "address": "HsNx7RirehVMy54xnFtcgCBPDMrwNnJKykageqdWpump"},
-#    {"name": "TRUMP", "address": "6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN"},
-# ]
-
 
 def get_token_list() -> list:
     """
@@ -41,38 +35,16 @@ def fetch_market_data(dummy: dict) -> str:
     token_list = get_token_list()
     market_data = []
     for token in token_list:
-        response = requests.get(
-            f"https://api.dexscreener.com/tokens/v1/solana/{token['address']}"
-        )
+        response = requests.get(f"https://api.dexscreener.com/tokens/v1/solana/{token['address']}")
         if response.status_code == 200:
             data = response.json()
+            # Remove unrelated data to fit the context limit
             if "info" in data[0]:
                 del data[0]["info"]
             if "url" in data[0]:
                 del data[0]["url"]
             market_data.append(data[0])
     return json.dumps(market_data)
-
-
-@tool
-def format_output(user: str, operation: str, token: str, amount: float) -> str:
-    """
-    Formats the output in a JSON string.
-
-    Args:
-        user: The user's name.
-        operation: The operation (Buy, Sell or Hold) to perform.
-        token: The token symbol.
-        amount: The amount to trade.
-
-    Returns:
-        A JSON string with the formatted output.
-    """
-    if operation not in ["Buy", "Sell", "Hold"]:
-        return json.dumps({"error": "Invalid operation."})
-    return json.dumps(
-        {"user": user, "operation": operation, "token": token, "amount": amount}
-    )
 
 
 if __name__ == "__main__":
