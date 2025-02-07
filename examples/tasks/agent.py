df345f6f6b31d99b2dc19bb4d8145f5fec0aed51
Maciej Witowski <maciej.witowski@protocol.ai>
2025-02-07 12:32:25 +0100

    Simplify the task agent example

--- examples/tasks/agent.py
+++ examples/tasks/agent.py
@@ -1,36 +1,65 @@
 import asyncio
 import os
+from pathlib import Path
 
-from galadriel import AgentRuntime
+from dotenv import load_dotenv
+
+from galadriel import AgentRuntime, CodeAgent
 from galadriel.clients import SimpleMessageClient
 from galadriel.clients import TwitterMentionClient
 from galadriel.clients.twitter_mention_client import TwitterCredentials
+from galadriel.core_agent import LiteLLMModel
 from galadriel.entities import Pricing
-from research_agent import research_agent
+from galadriel.tools.web3 import dexscreener, coingecko
 
-twitter_client = TwitterMentionClient(
-    TwitterCredentials(
-        consumer_api_key=os.getenv("TWITTER_CONSUMER_API_KEY"),
-        consumer_api_secret=os.getenv("TWITTER_CONSUMER_API_SECRET"),
-        access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
-        access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
-    ),
-    user_id=os.getenv("TWITTER_USER_ID"),
+load_dotenv(dotenv_path=Path(".") / ".env", override=True)
+
+model = LiteLLMModel(
+    model_id="openai/gpt-4o",
+    api_key=os.getenv("OPENAI_API_KEY"),
 )
 
+# Setup a researcher who will perform Web3 related tasks
+researcher = CodeAgent(
+    tools=[
+        coingecko.get_coin_price,
+        dexscreener.get_token_profile,
+    ],
+    model=model,
+    add_base_tools=True,
+)
+
+# Configure agent's pricing information
+agent_pricing = Pricing(
+    wallet_address="5RYHzQuknP2viQjYzP27wXVWKeaxonZgMBPQA86GV92t", # Agent's wallet address
+    cost=0.001, # Price per task in SOL
+)
+
+# The client will pass a research task to the agent with a signature of transaction of Solana.
+# This transactions proves that
+# https://explorer.solana.com/tx/5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy
 simple_client = SimpleMessageClient(
     "Is Bitcoin good investment now with high prices? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
-    "Should I buy ETH at this dip? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
 )
 
+# Alternatively, Twitter (or Discord, Telegram) client which can be passed to inputs and outputs in order to get tasks from social channels
+# twitter_client = TwitterMentionClient(
+#     TwitterCredentials(
+#         consumer_api_key=os.getenv("TWITTER_CONSUMER_API_KEY"),
+#         consumer_api_secret=os.getenv("TWITTER_CONSUMER_API_SECRET"),
+#         access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+#         access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+#     ),
+#     user_id=os.getenv("TWITTER_USER_ID"),
+# )
+
+# Combine all elements into runtime
 runtime = AgentRuntime(
-    inputs=[twitter_client, simple_client],
-    outputs=[twitter_client, simple_client],
-    agent=research_agent,
-    pricing=Pricing(
-        wallet_address="5RYHzQuknP2viQjYzP27wXVWKeaxonZgMBPQA86GV92t",
-        cost=0.001,
-    ),
+    inputs=[simple_client], # Runtime inputs, pass twitter_client if you use it
+    outputs=[simple_client], # Runtime outputs, pass twitter_client if you use it
+    agent=researcher,
+    pricing=agent_pricing,
 )
 
+# Run the runtime with agent
 asyncio.run(runtime.run())
