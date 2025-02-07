7b0087adc51db6f7dd92a7a69a5b2e7957104d68
Maciej Witowski <maciej.witowski@protocol.ai>
2025-02-07 11:59:46 +0100

    Drop main() for consistency with other agents

--- examples/tasks/agent.py
+++ examples/tasks/agent.py
@@ -1,48 +1,37 @@
-import os
-import json
 import asyncio
+import os
 
 from galadriel import AgentRuntime
-
 from galadriel.clients import SimpleMessageClient
 from galadriel.clients import TwitterMentionClient
 from galadriel.clients.twitter_mention_client import TwitterCredentials
-from galadriel.entities import Message
 from galadriel.entities import Pricing
-from galadriel.logging_utils import get_agent_logger
 from research_agent import research_agent
 
-logger = get_agent_logger()
+twitter_client = TwitterMentionClient(
+    TwitterCredentials(
+        consumer_api_key=os.getenv("TWITTER_CONSUMER_API_KEY"),
+        consumer_api_secret=os.getenv("TWITTER_CONSUMER_API_SECRET"),
+        access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+        access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    ),
+    user_id=os.getenv("TWITTER_USER_ID"),
+    logger=logger,
+)
 
+simple_client = SimpleMessageClient(
+    "Is Bitcoin good investment now with high prices? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
+    "Should I buy ETH at this dip? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
+)
 
-async def main():
-    twitter_client = TwitterMentionClient(
-        TwitterCredentials(
-            consumer_api_key=os.getenv("TWITTER_CONSUMER_API_KEY"),
-            consumer_api_secret=os.getenv("TWITTER_CONSUMER_API_SECRET"),
-            access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
-            access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
-        ),
-        user_id=os.getenv("TWITTER_USER_ID"),
-        logger=logger,
-    )
+runtime = AgentRuntime(
+    inputs=[twitter_client, simple_client],
+    outputs=[twitter_client, simple_client],
+    agent=research_agent,
+    pricing=Pricing(
+        wallet_address="5RYHzQuknP2viQjYzP27wXVWKeaxonZgMBPQA86GV92t",
+        cost=0.001,
+    ),
+)
 
-    simple_client = SimpleMessageClient(
-        "Is Bitcoin good investment now with high prices? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
-        "Should I buy ETH at this dip? 5aqB4BGzQyFybjvKBjdcP8KAstZo81ooUZnf64vSbLLWbUqNSGgXWaGHNteiK2EJrjTmDKdLYHamJpdQBFevWuvy",
-    )
-
-    agent = AgentRuntime(
-        inputs=[twitter_client, simple_client],
-        outputs=[twitter_client, simple_client],
-        agent=research_agent,
-        pricing=Pricing(
-            wallet_address="5RYHzQuknP2viQjYzP27wXVWKeaxonZgMBPQA86GV92t",
-            cost=0.001,
-        ),
-    )
-    await agent.run()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(runtime.run())
