c7b8b129744b5111af51e9b24d2f3bd05a3aa556
Krešimir Grofelnik <kresimir.grofelnik@gmail.com>
2025-02-07 13:31:12 +0100

    [ENG-1196] Message routing (#50)
    * Multiple channels
    * dropped `agent.json` in `task_agent`

--- galadriel/clients/twitter_mention_client.py
+++ galadriel/clients/twitter_mention_client.py
@@ -1,33 +1,47 @@
+import logging
 from typing import Dict
 from typing import List
 from typing import Optional
 
-from galadriel import AgentInput, AgentOutput
+from galadriel.agent import AgentInput
+from galadriel.agent import AgentOutput
 from galadriel.connectors.twitter import TwitterApiClient
 from galadriel.connectors.twitter import TwitterCredentials
+from galadriel.entities import HumanMessage
 from galadriel.entities import Message
 from galadriel.entities import PushOnlyQueue
 
 
 class TwitterMentionClient(TwitterApiClient, AgentInput, AgentOutput):
-    def __init__(self, _credentials: TwitterCredentials, user_id: str):
+    def __init__(
+        self, _credentials: TwitterCredentials, user_id: str, logger: logging.Logger
+    ):
         super().__init__(_credentials)
         self.user_id = user_id
+        self.logger = logger
 
     async def start(self, queue: PushOnlyQueue) -> None:
         mentions = await self._fetch_mentions(self.user_id)
         for mention in mentions:
-            message = Message(content=mention)
+            message = HumanMessage(
+                content=mention,
+                conversation_id=mention["conversation_id"],
+                additional_kwargs={
+                    "tweet_id": mention["tweet_id"],
+                    "author": mention["author_id"],
+                },
+            )
             await queue.put(message)
 
     async def send(self, request: Message, response: Message) -> None:
-        await self._post_reply(
-            response.additional_kwargs["reply_to_id"], response.content
-        )
+        if request.additional_kwargs and (
+            tweet_id := request.additional_kwargs.get("tweet_id")
+        ):
+            await self._post_reply(tweet_id, response.content)
 
     async def _fetch_mentions(self, user_id: str) -> List[Dict]:
         try:
-            response = await self._make_request(
+            response = self._make_request(
                 "GET",
                 f"users/{user_id}/mentions",
                 params={
@@ -37,17 +51,16 @@ class TwitterMentionClient(TwitterApiClient, AgentInput, AgentOutput):
                 },
             )
             tweets = response.get("data", [])
-            print(tweets)
             return tweets
         except Exception as e:
-            print(f"Error fetching mentions: {e}")
+            self.logger.error(f"Failed to fetch mentions: {e}")
             return []
 
     async def _post_reply(self, reply_to_id: str, message: str) -> Optional[Dict]:
-        response = await self._make_request(
+        response = self._make_request(
             "POST",
             "tweets",
             json={"text": message, "reply": {"in_reply_to_tweet_id": reply_to_id}},
         )
-        print(f"Tweet posted successfully: {message}")
+        self.logger.info(f"Tweet posted successfully: {message}")
         return response
