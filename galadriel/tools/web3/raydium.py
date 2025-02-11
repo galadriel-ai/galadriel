66bcb16e2950c513e760a2e6d9e2a8526c1564fc
Han <windzhaohan@gmail.com>
2025-02-11 09:12:28 +0100

    Reorder functions for better readability

--- galadriel/tools/web3/raydium.py
+++ galadriel/tools/web3/raydium.py
@@ -220,222 +220,6 @@ class SellTokenForSolTool(WalletTool):
         return result
 
 
-def fetch_amm_v4_pool_keys(pair_address: str) -> Optional[AmmV4PoolKeys]:
-
-    def bytes_of(value):
-        if not (0 <= value < 2**64):
-            raise ValueError("Value must be in the range of a u64 (0 to 2^64 - 1).")
-        return struct.pack("<Q", value)
-
-    try:
-        amm_id = Pubkey.from_string(pair_address)
-        amm_data = client.get_account_info_json_parsed(
-            amm_id, commitment=Processed
-        ).value.data
-        amm_data_decoded = LIQUIDITY_STATE_LAYOUT_V4.parse(amm_data)
-        marketId = Pubkey.from_bytes(amm_data_decoded.serumMarket)
-        marketInfo = client.get_account_info_json_parsed(
-            marketId, commitment=Processed
-        ).value.data
-        market_decoded = MARKET_STATE_LAYOUT_V3.parse(marketInfo)
-        vault_signer_nonce = market_decoded.vault_signer_nonce
-
-        ray_authority_v4 = Pubkey.from_string(
-            "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1"
-        )
-        open_book_program = Pubkey.from_string(
-            "srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX"
-        )
-
-        pool_keys = AmmV4PoolKeys(
-            amm_id=amm_id,
-            base_mint=Pubkey.from_bytes(market_decoded.base_mint),
-            quote_mint=Pubkey.from_bytes(market_decoded.quote_mint),
-            base_decimals=amm_data_decoded.coinDecimals,
-            quote_decimals=amm_data_decoded.pcDecimals,
-            open_orders=Pubkey.from_bytes(amm_data_decoded.ammOpenOrders),
-            target_orders=Pubkey.from_bytes(amm_data_decoded.ammTargetOrders),
-            base_vault=Pubkey.from_bytes(amm_data_decoded.poolCoinTokenAccount),
-            quote_vault=Pubkey.from_bytes(amm_data_decoded.poolPcTokenAccount),
-            market_id=marketId,
-            market_authority=Pubkey.create_program_address(
-                seeds=[bytes(marketId), bytes_of(vault_signer_nonce)],
-                program_id=open_book_program,
-            ),
-            market_base_vault=Pubkey.from_bytes(market_decoded.base_vault),
-            market_quote_vault=Pubkey.from_bytes(market_decoded.quote_vault),
-            bids=Pubkey.from_bytes(market_decoded.bids),
-            asks=Pubkey.from_bytes(market_decoded.asks),
-            event_queue=Pubkey.from_bytes(market_decoded.event_queue),
-            ray_authority_v4=ray_authority_v4,
-            open_book_program=open_book_program,
-            token_program_id=TOKEN_PROGRAM_ID,
-        )
-
-        return pool_keys
-    except Exception as e:
-        logger.error(f"Error fetching pool keys: {e}")
-        return None
-
-
-def get_amm_v4_reserves(pool_keys: AmmV4PoolKeys) -> tuple:
-    try:
-        quote_vault = pool_keys.quote_vault
-        quote_decimal = pool_keys.quote_decimals
-        quote_mint = pool_keys.quote_mint
-
-        base_vault = pool_keys.base_vault
-        base_decimal = pool_keys.base_decimals
-        base_mint = pool_keys.base_mint
-
-        balances_response = client.get_multiple_accounts_json_parsed(
-            [quote_vault, base_vault], Processed
-        )
-        balances = balances_response.value
-
-        quote_account = balances[0]
-        base_account = balances[1]
-
-        quote_account_balance = quote_account.data.parsed["info"]["tokenAmount"][
-            "uiAmount"
-        ]
-        base_account_balance = base_account.data.parsed["info"]["tokenAmount"][
-            "uiAmount"
-        ]
-
-        if quote_account_balance is None or base_account_balance is None:
-            logger.error("Error: One of the account balances is None.")
-            return None, None, None
-
-        if base_mint == WSOL:
-            base_reserve = quote_account_balance
-            quote_reserve = base_account_balance
-            token_decimal = quote_decimal
-        else:
-            base_reserve = base_account_balance
-            quote_reserve = quote_account_balance
-            token_decimal = base_decimal
-
-        logger.info(f"Base Mint: {base_mint} | Quote Mint: {quote_mint}")
-        logger.info(
-            f"Base Reserve: {base_reserve} | Quote Reserve: {quote_reserve} | Token Decimal: {token_decimal}"
-        )
-        return base_reserve, quote_reserve, token_decimal
-
-    except Exception as e:
-        logger.error(f"Error occurred: {e}")
-        return None, None, None
-
-
-def make_amm_v4_swap_instruction(
-    amount_in: int,
-    minimum_amount_out: int,
-    token_account_in: Pubkey,
-    token_account_out: Pubkey,
-    accounts: AmmV4PoolKeys,
-    owner: Pubkey,
-) -> Instruction:
-    try:
-
-        keys = [
-            AccountMeta(
-                pubkey=accounts.token_program_id, is_signer=False, is_writable=False
-            ),
-            AccountMeta(pubkey=accounts.amm_id, is_signer=False, is_writable=True),
-            AccountMeta(
-                pubkey=accounts.ray_authority_v4, is_signer=False, is_writable=False
-            ),
-            AccountMeta(pubkey=accounts.open_orders, is_signer=False, is_writable=True),
-            AccountMeta(
-                pubkey=accounts.target_orders, is_signer=False, is_writable=True
-            ),
-            AccountMeta(pubkey=accounts.base_vault, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=accounts.quote_vault, is_signer=False, is_writable=True),
-            AccountMeta(
-                pubkey=accounts.open_book_program, is_signer=False, is_writable=False
-            ),
-            AccountMeta(pubkey=accounts.market_id, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=accounts.bids, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=accounts.asks, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=accounts.event_queue, is_signer=False, is_writable=True),
-            AccountMeta(
-                pubkey=accounts.market_base_vault, is_signer=False, is_writable=True
-            ),
-            AccountMeta(
-                pubkey=accounts.market_quote_vault, is_signer=False, is_writable=True
-            ),
-            AccountMeta(
-                pubkey=accounts.market_authority, is_signer=False, is_writable=False
-            ),
-            AccountMeta(pubkey=token_account_in, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=token_account_out, is_signer=False, is_writable=True),
-            AccountMeta(pubkey=owner, is_signer=True, is_writable=False),
-        ]
-
-        data = bytearray()
-        discriminator = 9
-        data.extend(struct.pack("<B", discriminator))
-        data.extend(struct.pack("<Q", amount_in))
-        data.extend(struct.pack("<Q", minimum_amount_out))
-        swap_instruction = Instruction(RAYDIUM_AMM_V4, bytes(data), keys)
-
-        return swap_instruction
-    except Exception as e:
-        logger.error(f"Error occurred: {e}")
-        return None
-
-
-def get_token_balance(pubkey: Pubkey, mint_str: str) -> float | None:
-
-    mint = Pubkey.from_string(mint_str)
-    response = client.get_token_accounts_by_owner_json_parsed(
-        pubkey, TokenAccountOpts(mint=mint), commitment=Processed
-    )
-
-    if response.value:
-        accounts = response.value
-        if accounts:
-            token_amount = accounts[0].account.data.parsed["info"]["tokenAmount"][
-                "uiAmount"
-            ]
-            if token_amount:
-                return float(token_amount)
-    return None
-
-
-def confirm_txn(
-    txn_sig: Signature, max_retries: int = 20, retry_interval: int = 3
-) -> bool:
-    retries = 1
-
-    while retries < max_retries:
-        try:
-            txn_res = client.get_transaction(
-                txn_sig,
-                encoding="json",
-                commitment=Confirmed,
-                max_supported_transaction_version=0,
-            )
-
-            txn_json = json.loads(txn_res.value.transaction.meta.to_json())
-
-            if txn_json["err"] is None:
-                logger.info("Transaction confirmed... try count:", retries)
-                return True
-
-            logger.error("Error: Transaction not confirmed. Retrying...")
-            if txn_json["err"]:
-                logger.error("Transaction failed.")
-                return False
-        except Exception as e:
-            logger.info("Awaiting confirmation... try count:", retries)
-            retries += 1
-            time.sleep(retry_interval)
-
-    logger.error("Max retries reached. Transaction confirmation failed.")
-    return None
-
-
 def buy(
     payer_keypair: Keypair, pair_address: str, sol_in: float = 0.01, slippage: int = 5
 ) -> str:
@@ -687,6 +471,192 @@ def tokens_for_sol(
     )
     sol_received = quote_vault_balance - updated_quote_vault_balance
     return round(sol_received, 9)
+
+
+def fetch_amm_v4_pool_keys(pair_address: str) -> Optional[AmmV4PoolKeys]:
+
+    def bytes_of(value):
+        if not (0 <= value < 2**64):
+            raise ValueError("Value must be in the range of a u64 (0 to 2^64 - 1).")
+        return struct.pack("<Q", value)
+
+    try:
+        amm_id = Pubkey.from_string(pair_address)
+        amm_data = client.get_account_info_json_parsed(amm_id, commitment=Processed).value.data
+        amm_data_decoded = LIQUIDITY_STATE_LAYOUT_V4.parse(amm_data)
+        marketId = Pubkey.from_bytes(amm_data_decoded.serumMarket)
+        marketInfo = client.get_account_info_json_parsed(marketId, commitment=Processed).value.data
+        market_decoded = MARKET_STATE_LAYOUT_V3.parse(marketInfo)
+        vault_signer_nonce = market_decoded.vault_signer_nonce
+
+        ray_authority_v4 = Pubkey.from_string("5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1")
+        open_book_program = Pubkey.from_string("srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX")
+
+        pool_keys = AmmV4PoolKeys(
+            amm_id=amm_id,
+            base_mint=Pubkey.from_bytes(market_decoded.base_mint),
+            quote_mint=Pubkey.from_bytes(market_decoded.quote_mint),
+            base_decimals=amm_data_decoded.coinDecimals,
+            quote_decimals=amm_data_decoded.pcDecimals,
+            open_orders=Pubkey.from_bytes(amm_data_decoded.ammOpenOrders),
+            target_orders=Pubkey.from_bytes(amm_data_decoded.ammTargetOrders),
+            base_vault=Pubkey.from_bytes(amm_data_decoded.poolCoinTokenAccount),
+            quote_vault=Pubkey.from_bytes(amm_data_decoded.poolPcTokenAccount),
+            market_id=marketId,
+            market_authority=Pubkey.create_program_address(
+                seeds=[bytes(marketId), bytes_of(vault_signer_nonce)],
+                program_id=open_book_program,
+            ),
+            market_base_vault=Pubkey.from_bytes(market_decoded.base_vault),
+            market_quote_vault=Pubkey.from_bytes(market_decoded.quote_vault),
+            bids=Pubkey.from_bytes(market_decoded.bids),
+            asks=Pubkey.from_bytes(market_decoded.asks),
+            event_queue=Pubkey.from_bytes(market_decoded.event_queue),
+            ray_authority_v4=ray_authority_v4,
+            open_book_program=open_book_program,
+            token_program_id=TOKEN_PROGRAM_ID,
+        )
+
+        return pool_keys
+    except Exception as e:
+        logger.error(f"Error fetching pool keys: {e}")
+        return None
+
+
+def get_amm_v4_reserves(pool_keys: AmmV4PoolKeys) -> tuple:
+    try:
+        quote_vault = pool_keys.quote_vault
+        quote_decimal = pool_keys.quote_decimals
+        quote_mint = pool_keys.quote_mint
+
+        base_vault = pool_keys.base_vault
+        base_decimal = pool_keys.base_decimals
+        base_mint = pool_keys.base_mint
+
+        balances_response = client.get_multiple_accounts_json_parsed(
+            [quote_vault, base_vault], Processed
+        )
+        balances = balances_response.value
+
+        quote_account = balances[0]
+        base_account = balances[1]
+
+        quote_account_balance = quote_account.data.parsed["info"]["tokenAmount"]["uiAmount"]
+        base_account_balance = base_account.data.parsed["info"]["tokenAmount"]["uiAmount"]
+
+        if quote_account_balance is None or base_account_balance is None:
+            logger.error("Error: One of the account balances is None.")
+            return None, None, None
+
+        if base_mint == WSOL:
+            base_reserve = quote_account_balance
+            quote_reserve = base_account_balance
+            token_decimal = quote_decimal
+        else:
+            base_reserve = base_account_balance
+            quote_reserve = quote_account_balance
+            token_decimal = base_decimal
+
+        logger.info(f"Base Mint: {base_mint} | Quote Mint: {quote_mint}")
+        logger.info(
+            f"Base Reserve: {base_reserve} | Quote Reserve: {quote_reserve} | Token Decimal: {token_decimal}"
+        )
+        return base_reserve, quote_reserve, token_decimal
+
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        return None, None, None
+
+
+def make_amm_v4_swap_instruction(
+    amount_in: int,
+    minimum_amount_out: int,
+    token_account_in: Pubkey,
+    token_account_out: Pubkey,
+    accounts: AmmV4PoolKeys,
+    owner: Pubkey,
+) -> Instruction:
+    try:
+
+        keys = [
+            AccountMeta(pubkey=accounts.token_program_id, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=accounts.amm_id, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.ray_authority_v4, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=accounts.open_orders, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.target_orders, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.base_vault, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.quote_vault, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.open_book_program, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=accounts.market_id, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.bids, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.asks, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.event_queue, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.market_base_vault, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.market_quote_vault, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=accounts.market_authority, is_signer=False, is_writable=False),
+            AccountMeta(pubkey=token_account_in, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=token_account_out, is_signer=False, is_writable=True),
+            AccountMeta(pubkey=owner, is_signer=True, is_writable=False),
+        ]
+
+        data = bytearray()
+        discriminator = 9
+        data.extend(struct.pack("<B", discriminator))
+        data.extend(struct.pack("<Q", amount_in))
+        data.extend(struct.pack("<Q", minimum_amount_out))
+        swap_instruction = Instruction(RAYDIUM_AMM_V4, bytes(data), keys)
+
+        return swap_instruction
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        return None
+
+
+def get_token_balance(pubkey: Pubkey, mint_str: str) -> float | None:
+
+    mint = Pubkey.from_string(mint_str)
+    response = client.get_token_accounts_by_owner_json_parsed(
+        pubkey, TokenAccountOpts(mint=mint), commitment=Processed
+    )
+
+    if response.value:
+        accounts = response.value
+        if accounts:
+            token_amount = accounts[0].account.data.parsed["info"]["tokenAmount"]["uiAmount"]
+            if token_amount:
+                return float(token_amount)
+    return None
+
+
+def confirm_txn(txn_sig: Signature, max_retries: int = 20, retry_interval: int = 3) -> bool:
+    retries = 1
+
+    while retries < max_retries:
+        try:
+            txn_res = client.get_transaction(
+                txn_sig,
+                encoding="json",
+                commitment=Confirmed,
+                max_supported_transaction_version=0,
+            )
+
+            txn_json = json.loads(txn_res.value.transaction.meta.to_json())
+
+            if txn_json["err"] is None:
+                logger.info("Transaction confirmed... try count:", retries)
+                return True
+
+            logger.error("Error: Transaction not confirmed. Retrying...")
+            if txn_json["err"]:
+                logger.error("Transaction failed.")
+                return False
+        except Exception as e:
+            logger.info("Awaiting confirmation... try count:", retries)
+            retries += 1
+            time.sleep(retry_interval)
+
+    logger.error("Max retries reached. Transaction confirmation failed.")
+    return None
 
 
 # main function to run the code
