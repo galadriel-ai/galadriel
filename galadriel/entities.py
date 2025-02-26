319edec472b7369a6b45064d24906db55b40e76d
Maciej Witowski <maciej.witowski@protocol.ai>
2025-02-26 15:01:31 +0100

    Drop current message kinds

--- galadriel/entities.py
+++ galadriel/entities.py
@@ -16,15 +16,6 @@ class Message(BaseModel):
     type: Optional[str] = None
     additional_kwargs: Optional[Dict] = None
 
-
-class HumanMessage(Message):
-    type: str = "human"
-
-
-class AgentMessage(Message):
-    type: str = "agent"
-
-
 class PushOnlyQueue:
     def __init__(self, queue: asyncio.Queue):
         self._queue = queue
