5eddded0c30901b8365fa061c4fb45eb7e0ece7a
Han <windzhaohan@gmail.com>
2025-03-05 10:28:36 +0100

    Improve basic web3 example & add token search tool of dexscreener

--- examples/basic-web3/agent.py
+++ examples/basic-web3/agent.py
@@ -6,21 +6,27 @@ from dotenv import load_dotenv
 
 from galadriel import AgentRuntime, CodeAgent, LiteLLMModel
 from galadriel.clients import SimpleMessageClient
-from galadriel.tools import DuckDuckGoSearchTool
 from galadriel.tools.web3.market_data import dexscreener
+from galadriel.tools.web3.onchain.solana import raydium
+from galadriel.wallets.solana_wallet import SolanaWallet
+
 
 load_dotenv(dotenv_path=Path(".") / ".env", override=True)
 model = LiteLLMModel(model_id="gpt-4o", api_key=os.getenv("OPENAI_API_KEY"))
 
-# Add agent with GPT-4o model and access to web search and market data
+solana_wallet = SolanaWallet(key_path=os.getenv("SOLANA_KEY_PATH"))
+
+# Add agent with GPT-4o model and access to market data
 agent = CodeAgent(
     model=model,
-    tools=[DuckDuckGoSearchTool(), dexscreener.GetTokenDataTool()],
+    tools=[dexscreener.SearchTokenPairTool(), raydium.SwapTokenTool(solana_wallet)],
     additional_authorized_imports=["json"],
 )
 
 # Add basic client which sends two messages to the agent and prints agent's result
-client = SimpleMessageClient("What are top tokens on the market today?", "Should I buy ETH?")
+client = SimpleMessageClient(
+    "What is the price of DAIGE today? Buy 0.001 SOL of DAIGE if the price is below 0.5 USD"
+)
 
 # Set up the runtime
 runtime = AgentRuntime(
